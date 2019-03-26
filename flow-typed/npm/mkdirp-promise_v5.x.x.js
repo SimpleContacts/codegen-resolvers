fdb493a6206f2e17fb6ec9660c0bc75aab9d2d09
Vincent Driessen <me@nvie.com>
2019-03-26 21:43:33 +0100

    Add flow libdefs

---
+++ flow-typed/npm/mkdirp-promise_v5.x.x.js
@@ -0,0 +1,11 @@
+// flow-typed signature: 3169d5ae85e8f727e1c02b9682c780ac
+// flow-typed version: 20effe6c5d/mkdirp-promise_v5.x.x/flow_>=v0.25.x
+
+/**
+ * Flow libdef for mkdirp-promise
+ * by Vincent Driessen, 2019-01-04
+ */
+
+declare module "mkdirp-promise" {
+  declare export default (path: string) => Promise<void>;
+}
